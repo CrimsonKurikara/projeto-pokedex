7c1e74fe179b697c1f72af52cc81a3d2479b9035
CrimsonKurikara <gabriel_silva.2016@outlook.com>
2021-11-04 03:03:34 -0300

    Terminada os ejs

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const closeMessage = document.querySelector("#close")
 const message = document.querySelector("#message")
 
-closeMessage.addEventListener("click", function (){
+closeMessage.addEventListener("click", function () {
     message.style.display = "none"
 })
 
